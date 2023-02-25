c6be8acb37ed07e700014db745e243d9f2c59e87
Dylan Riffle <djriffle1@gmail.com>
2023-02-24 17:52:21 -0800

    firestore function clean up

--- services/FoodItemFirestoreService.ts
+++ services/FoodItemFirestoreService.ts
@@ -2,29 +2,19 @@ import { db } from '../firebase/firebaseApp';
 import { 
     collection,
     addDoc,
-    getDocs,
-    orderBy,
-    query,
     setDoc,
     deleteDoc,
-    getDoc,
     doc,
-    Timestamp,
-    where,
-    QuerySnapshot,
-    DocumentData,
-    DocumentReference,
-    QueryDocumentSnapshot,
  } from "firebase/firestore"; 
 import { FoodItem } from '../components/FoodItem';
 
 // Add a new document with a generated id.
 
 const FOOD_INVENTORY_COLLECTION = "foodInvetory";
-const DAILY_FOODS_COLLECTION = "dailyFoods";
-const DAILY_FOODS_SUB_COLLECTION = "dailyFoodItems";
 
 const FoodItemFirestoreService = {
+    
+
     uploadFoodItem: async (foodItem: FoodItem) => {
         const docRef = await addDoc(collection(db, FOOD_INVENTORY_COLLECTION), foodItem);
         console.log("Document written with ID: ", docRef.id);
@@ -39,28 +29,6 @@ const FoodItemFirestoreService = {
         await deleteDoc(doc(db, FOOD_INVENTORY_COLLECTION, foodItem.foodItemId));
         console.log("Document deleted from firestore")
     },
-    getAllFoodItems: async () : Promise<Array<FoodItem>> => {
-        const queryFoodInvetory = query(collection(db, FOOD_INVENTORY_COLLECTION), orderBy("addedToInventory", "desc"));
-        const foodInventorySnapshot = await getDocs(queryFoodInvetory);
-        let allFoodItems: Array<FoodItem> = [];
-        foodInventorySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-            allFoodItems = allFoodItems.concat({
-                foodItemId: doc.id,
-                name: doc.data().name,
-                brand: doc.data().brand,
-                serving_qty: doc.data().serving_qty,
-                serving_unit: doc.data().serving_unit,
-                num_servings: doc.data().num_servings,
-                image: doc.data().image,
-                calories: doc.data().calories,
-                protein: doc.data().protein,
-                fat: doc.data().fat,
-                carbs: doc.data().carbs,
-                addedToInventory: doc.data().addedToInventory,
-            });
-        });
-        return allFoodItems;
-    },
+
 }
 export default FoodItemFirestoreService
