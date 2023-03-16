466b34804a7dbd4e49f82f3de4028027b5819856
Dylan Riffle <djriffle1@gmail.com>
2023-03-15 17:42:43 -0700

    set tab icons

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -5,7 +5,11 @@
  */
 import { FontAwesome } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
 import { ColorSchemeName, Pressable } from 'react-native';
@@ -15,17 +19,26 @@ import ModalScreen from '../screens/ModalScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import TabOneScreen from '../screens/TabOneScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
-import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
+import {
+  RootStackParamList,
+  RootTabParamList,
+  RootTabScreenProps,
+} from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 import EditFoodScreen from '../screens/EditFoodScreen';
 import ProfilePageScreen from '../screens/ProfilePageScreen';
 import MealRecommendation from '../screens/MealRecommendation';
 
-export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+export default function Navigation({
+  colorScheme,
+}: {
+  colorScheme: ColorSchemeName;
+}) {
   return (
     <NavigationContainer
       linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+    >
       <RootNavigator />
     </NavigationContainer>
   );
@@ -39,21 +52,41 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
   return (
-      <Stack.Navigator>
-        <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
-        <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
-        <Stack.Group screenOptions={{ presentation: 'modal' }}>
-          <Stack.Screen name="Modal" component={ModalScreen} options={{
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Root"
+        component={BottomTabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="NotFound"
+        component={NotFoundScreen}
+        options={{ title: 'Oops!' }}
+      />
+      <Stack.Group screenOptions={{ presentation: 'modal' }}>
+        <Stack.Screen
+          name="Modal"
+          component={ModalScreen}
+          options={{
             title: 'Add Food',
-          }}/>
-          <Stack.Screen name="EditFoodScreen" component={EditFoodScreen} options={{
+          }}
+        />
+        <Stack.Screen
+          name="EditFoodScreen"
+          component={EditFoodScreen}
+          options={{
             title: 'Edit Food',
-          }}/>
-          <Stack.Screen name="ProfilePageScreen" component={ProfilePageScreen} options={{
+          }}
+        />
+        <Stack.Screen
+          name="ProfilePageScreen"
+          component={ProfilePageScreen}
+          options={{
             title: 'Profile Page',
-          }}/>
-        </Stack.Group>
-      </Stack.Navigator>
+          }}
+        />
+      </Stack.Group>
+    </Stack.Navigator>
   );
 }
 
@@ -71,25 +104,33 @@ function BottomTabNavigator() {
       initialRouteName="TabOne"
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme].tint,
-      }}>
+      }}
+    >
       <BottomTab.Screen
         name="TabOne"
         component={TabOneScreen}
         options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
           title: 'My Foods',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome
+              name="lemon-o"
+              size={25}
+              color={Colors[colorScheme].text}
+            />
+          ),
           headerRight: () => (
             <Pressable
               onPress={() => navigation.navigate('Modal')}
               style={({ pressed }) => ({
                 opacity: pressed ? 0.5 : 1,
-              })}>
-                <FontAwesome
-                  name="plus-circle"
-                  size={25}
-                  color={Colors[colorScheme].text}
-                  style={{ marginRight: 15 }}
-                />
+              })}
+            >
+              <FontAwesome
+                name="plus-circle"
+                size={25}
+                color={Colors[colorScheme].text}
+                style={{ marginRight: 15 }}
+              />
             </Pressable>
           ),
           headerLeft: () => (
@@ -97,44 +138,58 @@ function BottomTabNavigator() {
               onPress={() => navigation.navigate('ProfilePageScreen')}
               style={({ pressed }) => ({
                 opacity: pressed ? 0.5 : 1,
-              })}>
-                <FontAwesome
-                  name="user-circle"
-                  size={25}
-                  color={Colors[colorScheme].text}
-                  style={{ marginLeft: 15 }}
-                />
+              })}
+            >
+              <FontAwesome
+                name="user-circle"
+                size={25}
+                color={Colors[colorScheme].text}
+                style={{ marginLeft: 15 }}
+              />
             </Pressable>
-        )})}
+          ),
+        })}
       />
       <BottomTab.Screen
         name="TabTwo"
         component={TabTwoScreen}
         options={{
           title: "Today's Foods",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => (
+            //make a morning related font awesome icon
+            <FontAwesome
+              name="sun-o"
+              size={25}
+              color={Colors[colorScheme].text}
+            />
+          ),
         }}
       />
+
       <BottomTab.Screen
         name="MealRecommendation"
         component={MealRecommendation}
-        options={({ navigation }: RootTabScreenProps<'MealRecommendation'>) => ({
-          title: "Recommendations",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        options={({
+          navigation,
+        }: RootTabScreenProps<'MealRecommendation'>) => ({
+          title: 'Recommendations',
+          tabBarIcon: ({ color }) => <FontAwesome name="pie-chart" size={25} />,
           headerLeft: () => (
             <Pressable
               onPress={() => navigation.navigate('ProfilePageScreen')}
               style={({ pressed }) => ({
                 opacity: pressed ? 0.5 : 1,
-              })}>
-                <FontAwesome
-                  name="user-circle"
-                  size={25}
-                  color={Colors[colorScheme].text}
-                  style={{ marginLeft: 15 }}
-                />
+              })}
+            >
+              <FontAwesome
+                name="user-circle"
+                size={25}
+                color={Colors[colorScheme].text}
+                style={{ marginLeft: 15 }}
+              />
             </Pressable>
-          )})}
+          ),
+        })}
       />
     </BottomTab.Navigator>
   );
